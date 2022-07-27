e83c755c7726b7e3e753ff8222edb7493e414505
DaleWillemse <DaleWillemse000@gmail.com>
2022-07-27 20:32:59 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace PRG281_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboPayment.Items.Add("Cash");
+        }
+    }
+}
